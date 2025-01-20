07cfb514a935a5c13b1c7e176c90bd3c4c52d9b6
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-20 11:36:28 -0700

    generated text anthropic

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
-import {openai} from '@ai-sdk/openai';
+import {anthropic} from '@ai-sdk/anthropic';
+import { generateText } from 'ai';
 
-const model = openai('gpt-4');
+const model = anthropic('claude-3-5-sonnet-latest');
+
+export const answerMyQuestion = async (question: string) => {
+    const { text } = await generateText({
+        model,
+        prompt: question,
+    
+})
+
+
+    return text;
+}
+
+const answer = await answerMyQuestion("What is the capital of France?");
+console.log(answer);
